9985c5ece529448eed820b67798fd1b46ce77937
Albert <wuyichang1990@gmail.com>
2018-10-12 16:24:59 +0100

    encrypt the password with md5.

--- folderLocker/checkpwd.cs
+++ folderLocker/checkpwd.cs
@@ -6,6 +6,7 @@ using System.Data;
 using System.Data.SQLite;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -35,23 +36,34 @@ namespace folderLocker
       {
         MessageBox.Show("password is missing");
       }
-      else if (textBox1.Text ==Convert.ToString( dt.Rows[0][1])) 
-      {
-        status = true;
-        MessageBox.Show("The folder is unlocked.");
-        //delete the record;
-        string DEL = "DELETE FROM FolderInfo WHERE folderName =@folderName";
-        DbHelperSQLite.ExecuteSql(DEL, p);
-        //
-        SendKeys.SendWait("{f5}");
-        this.Close();
-      }
       else
       {
-        MessageBox.Show("password is incorrect.");
-        textBox1.Clear();
-        textBox1.Focus();
+        //the input password should be convert to md5 password;
+        MD5 md5 = new MD5CryptoServiceProvider();
+        byte[] plaintext = Encoding.Default.GetBytes(textBox1.Text);
+        byte[] ciphertext = md5.ComputeHash(plaintext);
+        string md5Pwd = Convert.ToBase64String(ciphertext);
+
+
+        if (md5Pwd == Convert.ToString(dt.Rows[0][1]))
+        {
+          status = true;
+          MessageBox.Show("The folder is unlocked.");
+          //delete the record;
+          string DEL = "DELETE FROM FolderInfo WHERE folderName =@folderName";
+          DbHelperSQLite.ExecuteSql(DEL, p);
+          //
+          SendKeys.SendWait("{f5}");
+          this.Close();
+        }
+        else
+        {
+          MessageBox.Show("password is incorrect.");
+          textBox1.Clear();
+          textBox1.Focus();
+        }
       }
+      
     }
 
     private void button2_Click(object sender, EventArgs e)
--- folderLocker/setpassword.cs
+++ folderLocker/setpassword.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Data.SQLite;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -36,7 +37,15 @@ namespace folderLocker
         //the password is valid, save it to sqlite.
         string SQLinsert = "INSERT INTO FolderInfo (folderName,folderpwd) VALUES (@folderName,@folderpwd)";
         SQLiteParameter p1 = new SQLiteParameter("@folderName", path);
-        SQLiteParameter p2 = new SQLiteParameter("@folderpwd", inputpwd.Text);
+        //the password should be encrypted before storing.
+        MD5 md5 = new MD5CryptoServiceProvider();
+        byte[] plaintext  = Encoding.Default.GetBytes(inputpwd.Text);
+        byte[] ciphertext = md5.ComputeHash(plaintext);
+
+        string md5Pwd = Convert.ToBase64String(ciphertext);
+
+        //SQLiteParameter p2 = new SQLiteParameter("@folderpwd", inputpwd.Text);
+        SQLiteParameter p2 = new SQLiteParameter("@folderpwd", md5Pwd);
         DbHelperSQLite.ExecuteSql(SQLinsert,p1, p2);
         //
         MessageBox.Show("The folder is locked.");
